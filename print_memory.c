ef630d60b8ecc802f2d8e32472c6cf58ca2397eb
VincentVimon <vinvimo@student.42.fr>
2018-03-23 19:10:07 +0100

    print_memory

--- print_memory.c
+++ print_memory.c
@@ -5,7 +5,7 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-void	ft_putoctal(short nbr)
+void	ft_puthexa(short nbr)
 {
 	if (nbr % 16 >= 0 && nbr % 16 < 10)
 		ft_putchar(nbr % 16 + '0');
@@ -32,6 +32,7 @@ void	print_dots(size_t index, short *nbr, size_t size)
 
 void	print_memory(const void *addr, size_t size)
 {
+	size_t	i;
 	size_t	index;
 	short	*nbr;
 
@@ -39,24 +40,34 @@ void	print_memory(const void *addr, size_t size)
 	index = 0;
 	while (index < size / sizeof(short))
 	{
-		ft_putoctal(nbr[index] >> 4);
-		ft_putoctal(nbr[index]);
+		ft_puthexa(nbr[index] >> 4);
+		ft_puthexa(nbr[index]);
 		if (size - index >= 2)
 		{
-			ft_putoctal(nbr[index] >> 12);
-			ft_putoctal(nbr[index] >> 8);
+			ft_puthexa(nbr[index] >> 12);
+			ft_puthexa(nbr[index] >> 8);
 		}
 		ft_putchar(' ');
-
 		index++;
 		if (index % 8 == 0)
 			print_dots(index - 8, nbr, index);
 	}
+	i = index;
+	while (i % 8 != 0)
+	{
+		ft_putchar(' ');
+		ft_putchar(' ');
+		ft_putchar(' ');
+		ft_putchar(' ');
+		ft_putchar(' ');
+		i++;
+	}
+	print_dots(index - (index % 8), nbr, index);
 }
 
 int		main(void)
 {
-	int tab[10] = {0, 23, 150, 255, 12, 16, 42, 103};
+	int tab[8] = {0, 23, 150, 255, 12, 16, 42, 103};
 
 	print_memory(tab, sizeof(tab));
 	return (0);
